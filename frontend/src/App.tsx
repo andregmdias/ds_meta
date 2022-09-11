6e1f445d45f687e84bd28ee8801dff2fd8dbd5ce
Andre Luiz <andregmdias@gmail.com>
2022-09-11 01:08:48 -0300

    feat: new components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import NotiifcationButton from "./components/NotiifcationButton";
 import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
       <Header/>
       <NotiifcationButton/>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard/>
+        </div>
+      </section>
     </>
   )
 }
